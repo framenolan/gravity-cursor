e51327540c07e336411f9a709c9b27622fef64de
Nolan Frame <frame.nolan@gmail.com>
2022-09-30 13:07:03 -0700

    initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import "./Button.css"
+
+function Button() {
+    
+       
+    
+    return (
+      <main className="Button">
+        <button className="Toggle"
+        // onClick={}
+        >Click Me</button>
+      </main>
+    );
+}
+
+export default Button;
